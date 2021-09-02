b5f673bef8348e9bea24005c534699f59647c31b
Sunik Kim <sunik.h.kim@gmail.com>
2021-09-02 14:34:29 -0400

    fleshing out routes, testing db queries

--- server/reviewsController.js
+++ server/reviewsController.js
@@ -1,8 +1,22 @@
 const { db } = require('../database/index');
 
 const reviews = (req, res) => {
-  console.log('hi reviews controller');
-  res.send('hi reviews controller');
+  let productId = req.params.productId;
+  console.log('REQUESTED product id: ', productId);
+
+  let query = 'SELECT * FROM reviews WHERE product_id=$1';
+  let query2 = 'SELECT url FROM reviews_photos WHERE review_id=$1';
+  let query3 = 'SELECT * FROM reviews_characteristics WHERE review_id=$1';
+  let query4 = 'SELECT name FROM products WHERE id=$1';
+  let query5 = 'SELECT name FROM characteristics WHERE product_id=$1';
+
+  db.any(query, productId)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 module.exports = {

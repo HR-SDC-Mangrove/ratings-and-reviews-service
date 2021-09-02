32fd3c801216194fa29131c4d3c579bfef87cb04
Sunik Kim <sunik.h.kim@gmail.com>
2021-09-02 14:42:54 -0400

    made sql improvements

--- server/reviewsController.js
+++ server/reviewsController.js
@@ -5,13 +5,14 @@ const reviews = (req, res) => {
   console.log('REQUESTED product id: ', productId);
 
   let query = 'SELECT * FROM reviews WHERE product_id=$1';
-  let query2 = 'SELECT url FROM reviews_photos WHERE review_id=$1';
+  let query2 = 'SELECT * FROM reviews_photos WHERE review_id=$1';
   let query3 = 'SELECT * FROM reviews_characteristics WHERE review_id=$1';
   let query4 = 'SELECT name FROM products WHERE id=$1';
-  let query5 = 'SELECT name FROM characteristics WHERE product_id=$1';
+  let query5 = 'SELECT name FROM characteristics WHERE id=$1';
 
   db.any(query, productId)
     .then(res => {
+      // add evergreen/default data for failed requests/faulty data
       console.log(res);
     })
     .catch(err => {

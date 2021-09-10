a14da62c262fceddf9e56b8e65b4ee7cf16b172a
Sunik Kim <sunik.h.kim@gmail.com>
2021-09-09 23:48:23 -0400

    added environment variable

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+/* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 const app = require('./server');
-require('newrelic');
+
+if (process.env.RELIC) {
+  require('newrelic');
+}
 
 app.listen(process.env.PORT, () => {
   console.log('App listening on port', process.env.PORT);

a41950598d7b7e3287cfeb4429931b64fe4528fb
Sunik Kim <sunik.h.kim@gmail.com>
2021-09-23 23:08:46 -0400

    added loader verification

--- server/server.js
+++ server/server.js
@@ -14,4 +14,16 @@ const reviewsRouter = require('./reviewsRoutes');
 
 app.use('/reviews', reviewsRouter);
 
+const loaderIO = (req, res) => {
+  console.log('entered loader');
+
+  res.sendFile(process.env.LOADER);
+};
+
+app.get('/loaderio-d37ce2bdeb6d2d663531cf1c51a39bd9.txt', loaderIO);
+
+app.get('/loaderio-d37ce2bdeb6d2d663531cf1c51a39bd9.html', loaderIO);
+
+app.get('/loaderio-d37ce2bdeb6d2d663531cf1c51a39bd9/', loaderIO);
+
 module.exports = app;
